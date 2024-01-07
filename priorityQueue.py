f2c90e595a05fb7a3ddeb2d77d83e31cece1774f
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-07 19:47:48 +0000

    feat: Completed priorityQueue

--- priorityQueue.py
+++ priorityQueue.py
@@ -23,3 +23,30 @@ def insert(array, newNum):
         array.append(newNum)
         for i in range((size // 2) - 1, -1, -1):
             heapify(array, size, i)
+
+def deleteNode(array, num):
+    size = len(array)
+    i = 0
+    for i in range(0, size):
+        if num == array[i]:
+            break
+
+    array[i], array[size - 1] = array[size - 1], array[i]
+
+    array.remove(size - 1)
+
+    for i in range((len(array) // 2) -1, -1, -1):
+        heapify(array, len(array), i)  
+
+arr = []
+
+insert(arr, 3)
+insert(arr, 4)
+insert(arr, 9)
+insert(arr, 5)
+insert(arr, 2)
+
+print ("Max-Heap array: " + str(arr))
+
+deleteNode(arr, 4)
+print("After deleting an element: " + str(arr))
